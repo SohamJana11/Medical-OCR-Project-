b6d6d316745f365979f43fd705f918bee3767b60
Soham Jana <162604344+SohamJana11@users.noreply.github.com>
2024-03-14 22:06:13 +0530

    Delete src directory

--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-#In simple terms, adding an __init__.py file to a directory is like 
-#putting a sign outside a building that tells people it's open for business.
-#Similarly, in Python, when you create an empty file called __init__.py inside a directory, 
-#it tells Python that this directory should be treated as a Python package.
