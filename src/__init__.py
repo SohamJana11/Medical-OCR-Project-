26a166673ed0be5681f59e8c7258032a5669f58e
Soham Jana <162604344+SohamJana11@users.noreply.github.com>
2024-03-15 11:07:19 +0530

    Add files via upload

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+#In simple terms, adding an __init__.py file to a directory is like 
+#putting a sign outside a building that tells people it's open for business.
+#Similarly, in Python, when you create an empty file called __init__.py inside a directory, 
+#it tells Python that this directory should be treated as a Python package.
